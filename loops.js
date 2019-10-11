535fe2ec5d92172c04e984029ff17a0b5533a2ba
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 18:57:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 function forLoop(arr) {
   for (let i=0; i < 25; i++){
     let isZero = (i === 0);
-    let val = `I am ${isZero ? "1" : ++i} strange loop${isZero ? "" : "s"}`;
+    let val = `I am ${isZero ? "1" : i} strange loop${isZero ? "" : "s"}`;
     arr.push(val);
   }
   return arr;
