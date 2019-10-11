df4cd30438f467efc415b877fc70eeee29a610da
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 18:59:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 function forLoop(arr) {
   for (let i=0; i < 25; i++){
     let isOne = (i === 1);
-    let val = `I am ${isOne ? "1" : i} strange loop${isOne ? "" : "s"}`;
+    let val = `I am ${isOne ? "1" : i} strange loop${isOne ? "" : "s"}.`;
     arr.push(val);
   }
   return arr;
