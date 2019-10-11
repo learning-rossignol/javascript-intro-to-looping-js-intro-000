4c080cab4feed48ae142638306a10466ac2c3d3c
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 18:45:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@
 function forLoop(arr) {
   for (var i=0; i < 25; i++){
     arr.push(
-         if (i === 1) {
+         if (i === 0) {
            "I am 1 strange loop."
          } else {
-           "I am ${i} strange loops."
+           `"I am ${i} strange loops."``
          }
   }
   return arr;
